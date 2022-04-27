9244c72ead6147bb4575f29f3f2777181888f9e8
nfagan <nick@fagan.org>
2022-04-27 13:18:32 -0400

    add classification script

---
+++ scripts/binary_fitcdiscr_perm_test.m
@@ -0,0 +1,52 @@
+function [model_p, real_p, null_ps] = binary_fitcdiscr_perm_test(data, ind_a, ind_b, varargin)
+
+defaults = struct();
+defaults.hold_out = 0.1;
+defaults.iters = 1e2;
+params = shared_utils.general.parsestruct( defaults, varargin );
+
+hold_out = params.hold_out;
+iters = params.iters;
+
+real_p = one_iter( data, ind_a, ind_b, hold_out );
+null_ps = zeros( iters, 1 );
+
+for i = 1:iters      
+  inds = [ind_a; ind_b];
+  inds = inds(randperm(numel(inds)));
+  null_ind_a = inds(1:numel(ind_a));
+  null_ind_b = inds(numel(ind_a)+1:end);
+  null_ps(i) = one_iter( data, null_ind_a, null_ind_b, hold_out );
+end
+
+model_p = 1 - pnz( real_p > null_ps );
+
+end
+
+function p_match = one_iter(data, ind_a, ind_b, hold_out)
+
+keep_in = 1 - hold_out;
+num_a = max( 1, floor(numel(ind_a) * keep_in) );
+num_b = max( 1, floor(numel(ind_b) * keep_in) );
+
+ind_aa = ind_a(randperm(numel(ind_a), num_a));
+ind_bb = ind_b(randperm(numel(ind_b), num_b));
+
+[train_Y, train_I] = combine_indices( ind_aa, ind_bb );
+train_X = data(train_I);
+
+rest_a = setdiff( ind_a, ind_aa );
+rest_b = setdiff( ind_b, ind_bb );
+[test_Y, test_I] = combine_indices( rest_a, rest_b );
+test_X = data(test_I);
+
+res = fitcdiscr( train_X, train_Y );
+pred = predict( res, test_X );
+p_match = sum( pred == test_Y ) / numel( test_Y );
+
+end
+
+function [y, i] = combine_indices(a, b)
+i = [ a; b ];
+y = [ ones(numel(a), 1); zeros(numel(b), 1) ];
+end
---
+++ scripts/discriminate_rois_nf.m
@@ -0,0 +1,61 @@
+data_p = '~/Downloads';
+
+sorted = shared_utils.io.fload( fullfile(data_p, 'sorted_neural_data_social_gaze.mat') );
+events = shared_utils.io.fload( fullfile(data_p, 'events.mat') );
+ct_labels = shared_utils.io.fload( fullfile(data_p, 'celltype_labels_combined.mat') );
+
+[spike_ts, spike_labels] = linearize_sorted( sorted );
+spike_labels = apply_cell_type_labels(...
+  spike_labels, ct_labels.label_mat, ct_labels.label_mat_cols );
+
+events = add_whole_face_whole_object_rois( events );
+evts = bfw.event_column( events, 'start_time' );
+
+%%  psth
+
+rois = { 'whole_face', 'right_nonsocial_object_whole_face_matched' };
+
+evt_mask = find( events.labels, [{'m1'}, rois] );
+spk_mask = find( spike_labels, 'valid-unit' );
+
+min_t = -0.5;
+max_t = 0.5;
+bin_width = 0.05;
+
+[psth_matrix, psth_labels, t] = compute_psth(...
+    spike_ts, spike_labels, spk_mask ...
+  , evts, events.labels, evt_mask ...
+  , min_t, max_t, bin_width );
+
+%%  discriminate between pairs of rois
+
+t_win = [0, 0.5];
+t_mask = t >= t_win(1) & t < t_win(2);
+t_mean = mean( psth_matrix(:, t_mask), 2 );
+
+rois = combs( psth_labels, 'roi' );
+roi_pairs = bfw.pair_combination_indices( numel(rois) );
+
+as = arrayfun( @(x) rois{x}, roi_pairs(:, 1), 'un', 0 );
+bs = arrayfun( @(x) rois{x}, roi_pairs(:, 2), 'un', 0 );
+
+% discriminate between a and b, for each a in as and b in bs
+each_I = findall( psth_labels, {'uuid', 'looks_by'} );
+[fit_res, fit_labels] = binary_fitcdiscrs( ...
+  t_mean, psth_labels, each_I, as, bs, 'roi' );
+
+%%  plot results
+
+% extract p value from classification
+ps = cellfun( @(x) x.p, fit_res );
+is_sig = ps < 0.05;
+
+% proportions of significant cells per region and roi
+mask = findnone( fit_labels, {'<cell-type>'} );
+[prop_labs, prop_I] = retaineach(...
+  fit_labels, {'region', 'looks_by', 'roi', 'cell-type'}, mask );
+props = proportions( is_sig, prop_I );
+
+pl = plotlabeled.make_common();
+axs = pl.bar( props, prop_labs, {}, 'region', {'roi', 'looks_by', 'cell-type'} );
+ylabel( axs(1), 'Prop Significant a vs b.' );
